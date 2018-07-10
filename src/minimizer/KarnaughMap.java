4538dc1848c2120751a4e5102f8c9b0e42e3cb96
loooph <loooph@gmx.de>
2018-07-10 21:20:52 +0200

    Grundlegende Gray-Code-Beschriftung

--- src/minimizer/KarnaughMap.java
+++ src/minimizer/KarnaughMap.java
@@ -4,6 +4,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,19 +65,21 @@ public class KarnaughMap {
 	public void draw(Graphics2D g, int x, int y, int width, int height) {
 		int cellwidth = width / (getXDim() + 1);
 		int cellheight = height / (getYDim() + 1);
+		Stroke thin = new BasicStroke(1.0f);
+		Stroke thick = new BasicStroke(1.5f);
 		
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
 		g.setColor(Color.BLACK);
-		g.setStroke(new BasicStroke(1.5f));
+		g.setStroke(thick);
 		g.drawRoundRect(x, y, width, height, cellwidth / 4, cellheight / 4);
 		g.drawLine(x + cellwidth, y, x + cellwidth, y + height);
 		g.drawLine(x, y + cellheight, x + width, y + cellheight);
 		
-		// cellwidth / 8 bzw. cellheight / 8 sind die Radien der Viertelellipsen in den Ecken, 0.293 ist 1 - 1 / sqrt(2)
+		// cellwidth / 8 bzw. cellheight / 8 sind die Radien der Viertelellipsen in den Ecken, 0.293 ist 1 - 1 / sqrt(2) gerundet
 		g.drawLine((int) ((x +  cellwidth / 8 * 0.293)) + 1, (int) (y + cellheight / 8 * 0.293) + 1, x + cellwidth, y + cellheight);
 		
-		g.setStroke(new BasicStroke(1));		
+		g.setStroke(thin);		
 		g.setColor(Color.GRAY);
 		for(int i = 2; i <= getXDim(); ++i) {
 			g.drawLine(x + i * cellwidth, y, x + i * cellwidth, y + height);
@@ -84,5 +87,20 @@ public class KarnaughMap {
 		for(int i = 2; i <= getYDim(); ++i) {
 			g.drawLine(x, y + i * cellheight, x + width, y + i * cellheight);
 		}
+		// Gray-Code
+		for(int i = 0; i < getXDim(); ++i) {
+			g.drawString(getGrayCode(i, getXDim() - 1), x + (i + 1) * cellwidth + 3, y + 2 * cellheight / 3);
+		}
+		for(int i = 0; i < getYDim(); ++i) {
+			g.drawString(getGrayCode(i, getYDim() - 1), x + 3, y + (i + 1) * cellheight + 2 * cellheight / 3);
+		}
+	}
+	
+	private String getGrayCode(int i, int maxVal) {
+		String grayCode = Integer.toBinaryString(i ^ (i >> 1));
+		while(grayCode.length() < Integer.toBinaryString(maxVal).length()) {
+			grayCode = "0" + grayCode;
+		}
+		return grayCode;
 	}
 }
