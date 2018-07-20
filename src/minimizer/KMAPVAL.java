9c1ce461d8cf2fa12b9013c8f8c4a2aea1d71039
loooph <loooph@gmx.de>
2018-07-20 19:52:03 +0200

    Ausgabefunktion hinzugefügt

--- src/minimizer/KMAPVAL.java
+++ src/minimizer/KMAPVAL.java
@@ -1,5 +1,20 @@
 package minimizer;
 
 public enum KMAPVAL {
-	FALSE, TRUE, DONTCARE
+	FALSE, TRUE, DONTCARE;
+
+	@Override
+	public String toString() {
+		switch(this) {
+			case TRUE:
+				return "1";
+			case FALSE:
+				return "0";
+			case DONTCARE:
+				// groﬂes Phi
+				return "\u03A6";
+		}
+		return "";
+	}
+	
 }
