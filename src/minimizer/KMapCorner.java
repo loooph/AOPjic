d0b43863b65ff85013ae668b8ccf2ddd2d134135
loooph <loooph@gmx.de>
2018-07-28 12:50:38 +0200

    Text hinzugefügt

--- src/minimizer/KMapCorner.java
+++ src/minimizer/KMapCorner.java
@@ -1,7 +1,9 @@
 package minimizer;
 
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 
+// TODO Zeichnen
 public class KMapCorner extends JComponent {
 
 	/**
@@ -9,6 +11,22 @@ public class KMapCorner extends JComponent {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private JLabel rowDesc;
+	private JLabel colDesc;
 	
-
+	public KMapCorner(int vars) {
+		String str = "</html>";
+		for(int i = 0; i < (vars + 1) / 2; ++i) {
+			str = "X<sub>" + i + "</sub>" + str;
+		}
+		str = "<html>" + str;
+		colDesc = new JLabel(str);
+		
+		str = "</html>";
+		for(int i = (vars + 1) / 2; i < vars; ++i) {
+			str = "X<sub>" + i + "</sub>" + str;
+		}
+		str = "<html>" + str;
+		rowDesc = new JLabel(str);
+	}
 }
