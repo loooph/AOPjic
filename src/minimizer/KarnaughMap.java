45518cb9e065cfe328c047dd683f1f58104c295e
loooph <loooph@gmx.de>
2018-07-28 12:47:24 +0200

    Eckfeld hinzugefügt

--- src/minimizer/KarnaughMap.java
+++ src/minimizer/KarnaughMap.java
@@ -2,6 +2,8 @@ package minimizer;
 
 import java.awt.Font;
 
+import javax.swing.JScrollPane;
+
 // TODO Eckfeld
 public class KarnaughMap extends Map {
 	
@@ -9,8 +11,10 @@ public class KarnaughMap extends Map {
 		super(vars, font);
 		KMapHeader colHead = new KMapHeader(KMapHeader.HORIZONTAL, getGrid().getCellwidth(), getGrid().getCols(), font);
 		KMapHeader rowHead = new KMapHeader(KMapHeader.VERTICAL, getGrid().getCellheight(), getGrid().getLines(), font);
+		KMapCorner corner = new KMapCorner(vars);
 		setColumnHeaderView(colHead);
 		setRowHeaderView(rowHead);
+		setCorner(JScrollPane.UPPER_LEFT_CORNER, corner);
 		setCellheight(rowHead.getCellheight());
 		setCellwidth(colHead.getCellwidth());
 	}
