1b6be4a9b7a5c99e28dd4302a37cf9fc68e4c79c
loooph <loooph@gmx.de>
2018-07-28 12:17:49 +0200

    Klasse für Ecke der Karnaugh-Plan-Beschriftung

---
+++ src/minimizer/KMapCorner.java
@@ -0,0 +1,14 @@
+package minimizer;
+
+import javax.swing.JComponent;
+
+public class KMapCorner extends JComponent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
