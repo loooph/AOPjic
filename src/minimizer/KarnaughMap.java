c2942fb2be3103c6f7681a12ea687f14a52f0b67
loooph <loooph@gmx.de>
2018-07-30 00:03:30 +0200

    Zu wenig Platz für Beschriftung bei sehr kleinen Schriftgrößen behoben

--- src/minimizer/KarnaughMap.java
+++ src/minimizer/KarnaughMap.java
@@ -15,7 +15,7 @@ public class KarnaughMap extends Map {
 		KMapHeader rowHead = new KMapHeader(KMapHeader.VERTICAL, getGrid().getCellheight(), getGrid().getLines(), font);
 		KMapCorner corner = new KMapCorner(vars, font);
 		colHead.setCellheight((int) corner.getPreferredSize().getHeight());
-		rowHead.setCellwidth((int) corner.getPreferredSize().getWidth());
+		rowHead.setCellwidth(Integer.max((int) corner.getPreferredSize().getWidth(), rowHead.getCellwidth()));
 		setCorner(JScrollPane.UPPER_LEFT_CORNER, corner);
 		setColumnHeaderView(colHead);
 		setRowHeaderView(rowHead);
