44ec90fb0e8c065a8d81823d9ff133aa19678327
loooph <loooph@gmx.de>
2018-07-30 00:05:40 +0200

    Konvertierungsfunktion für Strings hinzugefügt

--- src/minimizer/KMAPVAL.java
+++ src/minimizer/KMAPVAL.java
@@ -17,4 +17,20 @@ public enum KMAPVAL {
 		return "";
 	}
 	
-}
+	public static KMAPVAL[] toKMAPVALArray(String str) {
+		KMAPVAL[] vals = new KMAPVAL[str.length()];
+		for(int i = 0; i < str.length(); ++i) {
+			switch(str.charAt(i)) {
+				case '0':
+					vals[i] = KMAPVAL.FALSE;
+					break;
+				case '1':
+					vals[i] = KMAPVAL.TRUE;
+					break;
+				default:
+					vals[i] = KMAPVAL.DONTCARE;
+					break;
+			}
+		}
+		return vals;	}
+	}
